9155e3d84ce4a45129df592caa82981c39561912
romarius-rich <rreutskij@gmail.com>
2024-11-13 12:41:16 +0300

    03-14

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -238,8 +238,15 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const finalMass = [];
+  let counter = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < arr.length; i++) {
+    counter += arr[i];
+    finalMass.push(counter);
+  }
+  return finalMass;
 }
 
 /**
