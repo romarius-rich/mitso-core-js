976a5c40cffb5fcd1ba5894b03212739543366c5
romarius-rich <rreutskij@gmail.com>
2024-11-13 12:46:52 +0300

    03-31

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -613,8 +613,14 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  return arr.length % 2 === 0
+    ? [...arr.slice(arr.length / 2), ...arr.slice(0, arr.length / 2)]
+    : [
+      ...arr.slice(Math.floor(arr.length / 2) + 1, arr.length),
+      arr[Math.floor(arr.length / 2)],
+      ...arr.slice(0, Math.floor(arr.length / 2)),
+    ];
 }
 
 module.exports = {
