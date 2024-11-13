67f72d08e27ad1d311386d4f6d6fcfbc89f6ffb1
romarius-rich <rreutskij@gmail.com>
2024-11-13 12:11:45 +0300

    01-10

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -165,8 +165,8 @@ function unbracketTag(str) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
