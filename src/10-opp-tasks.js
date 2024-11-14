fd65210d00f007ba64b226374587d861ca0bc5ab
romarius-rich <rreutskij@gmail.com>
2024-11-14 18:25:56 +0300

    10-1

--- src/10-opp-tasks.js
+++ src/10-opp-tasks.js
@@ -24,34 +24,42 @@
 class PaginationHelper {
   // The constructor takes in an array of items and a integer indicating how many
   // items fit within a single page
-  constructor(/* collection, itemsPerPage */) {
-    throw new Error('Not implemented');
+  constructor(collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
   }
 
   // returns the number of items within the entire collection
   itemCount() {
     console.log(this);
-    throw new Error('Not implemented');
+    return this.collection.length;
   }
 
   // returns the number of pages
   pageCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+    return Math.ceil(this.collection.length / this.itemsPerPage);
   }
 
   // returns the number of items on the current page. page_index is zero based.
   // this method should return -1 for pageIndex values that are out of range
-  pageItemCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+  pageItemCount(num) {
+    if (num < 0 || num >= this.pageCount()) {
+      return -1;
+    }
+    if (num === this.pageCount() - 1) {
+      return this.collection.length % this.itemsPerPage || this.itemsPerPage;
+    }
+    return this.itemsPerPage;
   }
 
   // determines what page an item is on. Zero based indexes
   // this method should return -1 for itemIndex values that are out of range
-  pageIndex() {
-    console.log(this);
-    throw new Error('Not implemented');
+  pageIndex(index) {
+    if (index < 0 || index >= this.collection.length) {
+      return -1;
+    }
+
+    return Math.floor(index / this.itemsPerPage);
   }
 }
 
