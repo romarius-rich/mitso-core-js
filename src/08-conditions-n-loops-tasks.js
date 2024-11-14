bee36467c29385c0c8af1c02ca2a8ddaf84c578d
romarius-rich <rreutskij@gmail.com>
2024-11-14 18:14:58 +0300

    08-3

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -72,8 +72,14 @@ function getFactorial(n) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let count = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let i = n1; i < n2 + 1; i++) {
+    // eslint-disable-next-line no-unused-vars
+    count += i;
+  }
+  return count;
 }
 
 /**
