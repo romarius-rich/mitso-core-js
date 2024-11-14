9cbe05eb5ce2297410a1d367bab4b770766f3a83
romarius-rich <rreutskij@gmail.com>
2024-11-14 18:22:53 +0300

    09-5

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -118,8 +118,20 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function (...args) {
+    let error;
+
+    for (let i = 0; i < attempts; i + 1) {
+      try {
+        return func.apply(this, args);
+      } catch (e) {
+        error = e;
+      }
+    }
+
+    throw error;
+  };
 }
 
 /**
