c76a1fedcf48973aa096b71f1d09c68c200f3352
romarius-rich <rreutskij@gmail.com>
2024-11-14 18:17:21 +0300

    08-8

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -226,8 +226,8 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  return `${isStartIncluded === true ? '[' : '('}${a < b ? a : b}, ${b > a ? b : a}${isEndIncluded === true ? ']' : ')'}`;
 }
 
 /**
